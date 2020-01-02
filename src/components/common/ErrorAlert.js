49e53fd5c94d32ff64dc7b79964e0ed5af2b745e
Pedro Alfonso <p.alfonsoag@gmail.com>
2020-01-01 20:59:19 -0400

    adding margin bottom

--- src/components/common/ErrorAlert.js
+++ src/components/common/ErrorAlert.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const ErrorAlert = ({ message }) => {
   return (
-    <p className="alert alert-danger py-3 text-center p-3 m-0">{message}</p>
+    <p className="alert alert-danger py-3 text-center p-3 m-0 mb-3">
+      {message}
+    </p>
   );
 };
 
