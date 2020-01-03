e9144ab62a93e1afe6ee20ab0189ad6ccc4e34a9
Pedro Alfonso <p.alfonsoag@gmail.com>
2020-01-02 22:44:11 -0400

    best buyers graphic

--- src/components/panel/ClientPanel.js
+++ src/components/panel/ClientPanel.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
 import { Query } from "react-apollo";
 import { TOP_TEN_BUYERS } from "../../queries";
 import Loader from "../common/Loader";
@@ -9,8 +10,33 @@ const ClientPanel = () => {
       {({ loading, error, data }) => {
         if (loading) return <Loader />;
         if (error) return `Error ${error.message}`;
-        console.log(data);
-        return <Loader />;
+        const topClientGraphic = [];
+
+        data.topClients.map((order, index) => {
+          topClientGraphic[index] = {
+            name:
+              order.client[0].name +
+              " " +
+              order.client[0].lastName.substring(0, 1),
+            total: order.total
+          };
+          return "";
+        });
+
+        return (
+          <BarChart
+            width={900}
+            height={400}
+            data={topClientGraphic}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="total" fill="#8884d8" />
+          </BarChart>
+        );
       }}
     </Query>
   );
